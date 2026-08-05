c8b822de10c3d1758dda0cea601acd22ced1395b
Jake Marshall <jake.marshall123@outlook.com>
2026-08-06 09:07:30 +1000

    Linting

--- src/ui/src/services/list-command-service.ts
+++ src/ui/src/services/list-command-service.ts
@@ -1,12 +1,10 @@
 import { Service, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
 import { map } from 'rxjs';
 
 @Service()
 export class ListCommandService {
   private http = inject(HttpClient);
-  private router = inject(Router);
 
   inititalizeList() {
     return this.http
@@ -19,15 +17,4 @@ export class ListCommandService {
       .post(`${listId}/add`, null, { responseType: 'text' })
       .pipe(map((res) => JSON.parse(res)));
   }
-  /*
-  inititalizeListItem(listId: string) {
-    this.http.post(`${listId}/add`, null, { responseType: 'text' }).subscribe({
-      next: response => {
-        const itemId = JSON.parse(response);
-        this.router.navigate([`${listId}/edit/${itemId}`]);
-      },
-      error: err =>
-    })
-  }
-  */
 }
