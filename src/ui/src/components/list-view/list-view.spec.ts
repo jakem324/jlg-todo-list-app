29ddd19ec05badcb667daf198df80edfb00f44fd
Jake Marshall <jake.marshall123@outlook.com>
2026-08-06 12:19:55 +1000

    Fixes test issue

--- src/ui/src/components/list-view/list-view.spec.ts
+++ src/ui/src/components/list-view/list-view.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { ListView } from './list-view';
 
@@ -9,6 +10,7 @@ describe('ListView', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ListView],
+      providers: [provideRouter([])]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ListView);
